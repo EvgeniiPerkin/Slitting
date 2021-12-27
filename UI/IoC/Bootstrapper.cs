fac5feedec84a97d8ec744cda9650d5544283b63
Перкин Евгений <evgenii.perkin@outlook.com>
2021-12-27 07:04:16 +0300

    save

--- UI/IoC/Bootstrapper.cs
+++ UI/IoC/Bootstrapper.cs
@@ -10,9 +10,15 @@ namespace UI.IoC
         public static IContainer Configure()
         {
             var builder = new ContainerBuilder();
+            var assemblies = new[] { Assembly.GetExecutingAssembly() };
 
             builder.RegisterType<LoadKnifes>().As<ILoader>();
             builder.RegisterType<LoadSpacers>().As<ILoader>();
+
+            builder.RegisterAssemblyTypes(assemblies)
+                .Where(t => typeof(IServices).IsAssignableFrom(t))
+                .SingleInstance()
+                .AsImplementedInterfaces();
 
             builder.RegisterAssemblyTypes(Assembly.Load(nameof(UI)))
                 .Where(t => t.Namespace.Contains("ViewModels"))
