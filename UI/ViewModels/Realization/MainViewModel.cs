72b74a365f83595ab49517e14cd87adbde1ba55f
Перкин Евгений <evgenii.perkin@outlook.com>
2021-12-30 22:47:18 +0300

    добавил в вью модель свойства и поля

--- UI/ViewModels/Realization/MainViewModel.cs
+++ UI/ViewModels/Realization/MainViewModel.cs
@@ -13,10 +13,23 @@ namespace UI.ViewModels.Realization
         {
             this.dialogServices = dialogServices;
             _Strips = new ObservableCollection<Strip>();
+            _Knifes = new ObservableCollection<Knife>();
         }
+        #region fields
         private readonly IDialogServices dialogServices;
         private ObservableCollection<Strip> _Strips;
-        /// <summary></summary>
+        private ObservableCollection<Knife> _Knifes;
+        private Knife _SelectedKnife;
+        private double _RollWidth;
+        private double _Thickness;
+        private int _CuttingMachine;
+        private double _Progress;
+        private string _Notify;
+        private string _Msg;
+        #endregion
+
+        #region properties
+        /// <summary>list strips</summary>
         public ObservableCollection<Strip> Strips
         {
             get => _Strips;
@@ -30,7 +43,121 @@ namespace UI.ViewModels.Realization
                 OnPropertyChanged();
             }
         }
+        /// <summary>list knifes</summary>
+        public ObservableCollection<Knife> Knifes
+        {
+            get => _Knifes;
+            set
+            {
+                if (_Knifes == value)
+                {
+                    return;
+                }
+                _Knifes = value;
+                OnPropertyChanged();
+            }
+        }
+        /// <summary>selected knife</summary>
+        public Knife SelectedKnife
+        {
+            get => _SelectedKnife;
+            set
+            {
+                if (_SelectedKnife == value)
+                {
+                    return;
+                }
+                _SelectedKnife = value;
+                OnPropertyChanged();
+            }
+        }
+        /// <summary>Roll width</summary>
+        public double RollWidth
+        {
+            get => _RollWidth;
+            set
+            {
+                if (_RollWidth == value)
+                {
+                    return;
+                }
+                _RollWidth = value;
+                OnPropertyChanged();
+            }
+        }
+        /// <summary>Thickness metal</summary>
+        public double Thickness
+        {
+            get => _Thickness;
+            set
+            {
+                if (_Thickness == value)
+                {
+                    return;
+                }
+                _Thickness = value;
+                OnPropertyChanged();
+            }
+        }
+        /// <summary>unoccupied сutting machine</summary>
+        public int CuttingMachine
+        {
+            get => _CuttingMachine;
+            set
+            {
+                if (_CuttingMachine == value)
+                {
+                    return;
+                }
+                _CuttingMachine = value;
+                OnPropertyChanged();
+            }
+        }
+        /// <summary>process execution scale</summary>
+        public double Progress
+        {
+            get => _Progress;
+            set
+            {
+                if (_Progress == value)
+                {
+                    return;
+                }
+                _Progress = value;
+                OnPropertyChanged();
+            }
+        }
+        /// <summary>message about the scale of execution</summary>
+        public string Notify
+        {
+            get => _Notify;
+            set
+            {
+                if (string.Compare(_Notify, value) == 0)
+                {
+                    return;
+                }
+                _Notify = value;
+                OnPropertyChanged();
+            }
+        }
+        /// <summary>help message about the current change</summary>
+        public string Msg
+        {
+            get => _Msg;
+            set
+            {
+                if (string.Compare(_Msg, value) == 0)
+                {
+                    return;
+                }
+                _Msg = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
 
+        #region commands
         private RelayCommand _Command;
         /// <summary>команда</summary>
         public RelayCommand Command => _Command ??
@@ -43,5 +170,7 @@ namespace UI.ViewModels.Realization
                         }
                         dialogServices.ShowMsg(result);
                     }));
+
+        #endregion
     }
 }
